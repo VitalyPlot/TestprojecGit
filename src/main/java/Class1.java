10e1b4bdf200312bb681ec8296be0323fbf217a0
VitalyPlot <98945406+VitalyPlot@users.noreply.github.com>
2023-07-14 06:41:44 +1000

    Update Class1.java

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,7 +1,7 @@
 public class Class1 {
     public static void main(String[] args) {
-        System.out.println("Text1");
-        System.out.println("Text3");
+        System.out.println("Text11");
+        System.out.println("Text13");
         System.out.println("Text4");
         System.out.println("Text5");
 
