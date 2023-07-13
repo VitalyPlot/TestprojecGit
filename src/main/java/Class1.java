75cf2c5566009451c5e8aa4e5aa0abd268c0d6f9
Vitaliy Plotnikov <vitaliyplot@gmail.com>
2023-07-14 05:51:50 +1000

    first new dranch commit

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -2,6 +2,8 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("Text1");
         System.out.println("Text3");
+        System.out.println("Text4");
+        System.out.println("Text5");
 
     }
 }
